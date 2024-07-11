b2274bebb47958092c12a5b95dec17529dcbe121
Abhishek Kumar <btech10872.23@bitmesra.ac.in>
2024-07-11 10:23:38 +0530

    added the about us part but still sometime to work on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import NavBar from "./components/Navbar"
+import About_us from "./components/About_us"
 
 function App() {
   return(
     <div>
       <NavBar/>
+      <About_us></About_us>
     </div>
   )
 }
