cb2e7cf6589d4ade4683dc2eb191f08967dd8a9e
RSAgr <142487331+RSAgr@users.noreply.github.com>
2024-07-12 09:27:47 +0000

    Blobs and homeImage

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -3,20 +3,23 @@ import { useState } from "react"
 import {Link} from "react-scroll"
 import {FaTimes} from "react-icons/fa"
 import { IoMenu } from "react-icons/io5";
-import logi from "../../assets/edcHome.jpg"
+import blobs from "../../assets/Vector.svg"
+
 import "./body.css"
 
 const Body = () => {
     return (
         <>
+        
         <div className="container">
         <h1 className="text">Igniting the innovation <span class="break"></span> within Upcoming Trendsetter</h1>
         <button className="btn">
             Let's Start
         </button>
+       
         </div>
         <div className="temp">
-            
+           
         </div>
         
         </>
