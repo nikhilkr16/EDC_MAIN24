884ff67815bdc7aeab0d238560cd5f05e2579daf
Ashish <118127894+Zenith-here@users.noreply.github.com>
2024-07-31 17:19:44 +0530

    added collage in teams

--- src/otherpages/Teamspage/teams.jsx
+++ src/otherpages/Teamspage/teams.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import Collage from './Collage'
 import sir1 from '../../assets/Sir1.png'
 import sir2 from '../../assets/VShah.png'
 import '../Teamspage/teams.css'
 import ujjwalS from '../../assets/ujjalSir.png'
 
+
 function Teamspage() {
     return (
         <>
+
+        
+        <Collage/>
+
+
+
+
+
             <div className=' p-10 hero-2'>
                 <div className='p-1'> <h3 className='text-xl font-semibold mb-4'><span className='bg-yellow-300 pt-2 pb-2 pl-4'>Ou</span>r Team</h3>
                     <h2 className='text-2xl font-bold mb-4 '>Faculty Member</h2>
