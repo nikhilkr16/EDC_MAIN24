ba2933de337cd408806dc02a487584f08a1caf9f
Anuj Nayak <anujnayak108@gmail.com>
2024-08-04 23:14:36 +0530

    Reapply "Merge branch 'main' of https://github.com/AnujNayak108/EDC_MAIN24"
    This reverts commit 2d0b14524e1ef28b7847d50afce9b6b6034835a5.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,18 +18,24 @@ const Footer=()=>{
                         <div className="para">
                         <p>Our aim is to ignite the flame of entrepreneurship in the minds of youth.</p>
                         <div className="socialmedia">
-                           <p><FaFacebook className="facebookIcon" /></p>
-                            <p><FaInstagram className="instagramIcon" ></FaInstagram></p>
-                            <p><FaLinkedinIn className="linkedinIcon"></FaLinkedinIn></p>
+                            <a href="https://www.facebook.com/edcbitmesra/" target="_blank" rel="noopener noreferrer">
+                              <FaFacebook className="facebookIcon" />
+                            </a>
+                            <a href="https://www.instagram.com/edcbitmesra/" target="_blank" rel="noopener noreferrer">
+                              <FaInstagram className="instagramIcon" />
+                            </a>
+                            <a href="https://www.linkedin.com/company/edcbitmesra/mycompany/" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedinIn className="linkedinIcon" />
+                            </a>
                         </div>
                         </div>
                     </div>
                     <div className="sb__footer-links_div">
                         <h3>QUICK LINK</h3>
-                        <a href="/Dean">
+                        <a href="https://www.linkedin.com/in/jeganathan-chockalingam-126003306/?originalSubdomain=in" target="_blank">
                            <p>Dean of RIE</p>
                         </a>
-                        <a href="/FA">
+                        <a href="https://www.linkedin.com/in/vishal-hshah/?originalSubdomain=in" target="_blank">
                            <p>Faculty Advisor</p>
                         </a>
                         <a href="/hall of Fame">
